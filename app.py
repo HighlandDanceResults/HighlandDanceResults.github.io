9bfff5f182051c1763a7a9625a4421d50e7c3a20
ejboomus <85642599+ejboomus@users.noreply.github.com>
2025-06-01 16:54:08 -0500

    creating app

--- app.py
+++ app.py
@@ -18,58 +18,161 @@ categories = ['A', 'B', 'C', 'D']
 values1 = np.random.randint(10, 100, size=4)
 values2 = np.random.randint(10, 100, size=4)
 
-# App layout
+data = pd.read_csv('/Users/ewood/Documents/GitHub/dance_website/emilys_app/data/test_all_new.csv', keep_default_na=False)
+df = pd.DataFrame(data)
+df.drop(df.columns[df.columns.str.contains('unnamed', case=False)], axis=1, inplace=True)
+df = df.replace('', np.NaN)
+
+DATA_TABLE_STYLE = {
+    "style_data_conditional": [
+        {"if": {"column_id": "Overall"}, "backgroundColor": "#e1eaf2"},
+        {
+            'if': {'row_index': 'odd'},
+            'backgroundColor': '#e1eaf2',
+        },
+        {
+            'if': {'column_id': 'Name'},
+            'textAlign': 'left'
+        }
+    ],
+    "style_header": {
+        "color": "black",
+        "backgroundColor": "#799DBF",
+        "fontWeight": "bold",
+    }
+}
+
+### --- PAGE LAYOUT --- ###
+navbar = html.Div(dbc.Card(dbc.CardHeader(html.Center(html.H1("Unofficial Highland Dance Results")))),
+                  style = {"margin-bottom": "0.3em"})
+
+
+dropdown_labels = list(df['Year'].unique())
+
+top_card = [
+    dbc.CardHeader(html.B("Select Data")),
+    dbc.CardBody([
+        dbc.Row([
+            dbc.Col([
+                html.P("1) Choose Year"),
+                dcc.Dropdown(dropdown_labels, id= 'year_dropdown', searchable=False)
+            ]),
+            dbc.Col([
+                html.P("2) Choose Competition"),
+                dcc.Dropdown('', id= 'comp_dropdown', searchable=False)
+            ]),
+            dbc.Col([
+                html.P("3) Choose Age Group"),
+                dcc.Dropdown('', id= 'age_dropdown', searchable=False)
+            ])
+        ]),
+        html.Br(),
+        dbc.Row([
+            html.Div([
+                html.Button('Submit', id = 'submit-btn',
+                            style = {"backgroundColor": "#e1eaf2"}
+                ),
+                html.Button('Reset Table', id = 'reset-btn',
+                            style = {"backgroundColor": "#e1eaf2", "color":"red"}
+                ),
+            ], style={'textAlign': 'center'})
+        ])
+    ])
+]
+
+cards = dbc.Container(
+    dbc.Col([
+        dbc.Row(dbc.Card(top_card, style= {"padding": "0px"})
+                , style = {"margin-bottom": "0.5em"}),
+        dbc.Row(
+            dbc.Card([
+                dbc.CardHeader("Results"),
+                dbc.CardBody([
+                    'Please select options first...'
+                    # dbc.Row(table_card),
+                    # dbc.Row(plot_card)
+                ],id = 'table_card')
+            ], style= {"padding": "0px", "margin-bottom": "0.5em"})
+        ),
+        dbc.Row(
+            dbc.Card([
+                dbc.CardHeader("Contact Us :)"),
+                dbc.CardBody([
+                    'Email us with results, questions, or concerns at highlanddanceresults@gmail.com'
+                ],id = 'contact_card')
+            ], style = {"margin-bottom": "0.5em"})
+            
+        )
+    ])
+, fluid=True, style= {"height": "80vh"})
+                
+
+
 app.layout = html.Div([
-    dcc.Graph(id='bar-chart'),
-    dcc.Dropdown(
-        id='chart-type-dropdown',
-        options=[
-            {'label': 'Grouped', 'value': 'group'},
-            {'label': 'Stacked', 'value': 'stack'}
-        ],
-        value='group',  # Default value
-        clearable=False,
-        style={'width': '50%'}
-    ),
-    dcc.Store(id='bar-data', data={'categories': categories, 'values1': list(values1), 'values2': list(values2)})
+    # dcc.Store(id = 'df_chosen'),
+    navbar,
+    cards
 ])
 
+
+
+
+
+
+
+
+# # App layout
+# app.layout = html.Div([
+#     dcc.Graph(id='bar-chart'),
+#     dcc.Dropdown(
+#         id='chart-type-dropdown',
+#         options=[
+#             {'label': 'Grouped', 'value': 'group'},
+#             {'label': 'Stacked', 'value': 'stack'}
+#         ],
+#         value='group',  # Default value
+#         clearable=False,
+#         style={'width': '50%'}
+#     ),
+#     dcc.Store(id='bar-data', data={'categories': categories, 'values1': list(values1), 'values2': list(values2)})
+# ])
+
 # Client-side callback (JavaScript function)
-app.clientside_callback(
-    """
-    function(chartType, data) {
-        var categories = data.categories;
-        var values1 = data.values1;
-        var values2 = data.values2;
+# app.clientside_callback(
+#     """
+#     function(chartType, data) {
+#         var categories = data.categories;
+#         var values1 = data.values1;
+#         var values2 = data.values2;
 
-        var barmode = chartType === 'stack' ? 'stack' : 'group';
+#         var barmode = chartType === 'stack' ? 'stack' : 'group';
 
-        return {
-            'data': [
-                {
-                    'x': categories,
-                    'y': values1,
-                    'type': 'bar',
-                    'name': 'Series 1'
-                },
-                {
-                    'x': categories,
-                    'y': values2,
-                    'type': 'bar',
-                    'name': 'Series 2'
-                }
-            ],
-            'layout': {
-                'title': `Bar Chart (${chartType === 'stack' ? 'Stacked' : 'Grouped'})`,
-                'barmode': barmode
-            }
-        };
-    }
-    """,
-    Output('bar-chart', 'figure'),
-    [Input('chart-type-dropdown', 'value')],
-    [Input('bar-data', 'data')]
-)
+#         return {
+#             'data': [
+#                 {
+#                     'x': categories,
+#                     'y': values1,
+#                     'type': 'bar',
+#                     'name': 'Series 1'
+#                 },
+#                 {
+#                     'x': categories,
+#                     'y': values2,
+#                     'type': 'bar',
+#                     'name': 'Series 2'
+#                 }
+#             ],
+#             'layout': {
+#                 'title': `Bar Chart (${chartType === 'stack' ? 'Stacked' : 'Grouped'})`,
+#                 'barmode': barmode
+#             }
+#         };
+#     }
+#     """,
+#     Output('bar-chart', 'figure'),
+#     [Input('chart-type-dropdown', 'value')],
+#     [Input('bar-data', 'data')]
+# )
 
 # app.clientside_callback(
 #     ClientsideFunction(
